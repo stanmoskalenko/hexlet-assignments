7dfd6d9c2a6a6b5df54ab02ba5316f1d79766c41
stanmoskalenko <st.moskalenko@gmail.com>
2023-06-21 15:25:02 +0300

    Simplified method

--- java-collections-ru/lists/src/main/java/exercise/App.java
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -7,28 +7,27 @@ import java.util.ListIterator;
 
 // BEGIN
 class App {
-    public static boolean scrabble(String randomSymbols, String word) {
+  public static boolean scrabble(String symbols, String word) {
 
-        if (randomSymbols.isEmpty() || word.isEmpty()) {
-            return false;
-        }
-
-        List<String> listRandomSymbols = new ArrayList<>(Arrays.stream(randomSymbols.toLowerCase().split("")).toList());
-        List<String> listWordSymbols = new ArrayList<>(Arrays.stream(word.toLowerCase().split("")).toList());
-        ArrayList<String> result = new ArrayList<>();
-
-        ListIterator<String> iter = listWordSymbols.listIterator();
-        var element = "";
-
-        while (iter.hasNext()) {
-            element = iter.next();
-            if (listRandomSymbols.contains(element)) {
-                listRandomSymbols.remove(element);
-                result.add(element);
-            }
-        }
-
-        return String.join("", result).equalsIgnoreCase(word);
+    if (symbols.isEmpty() || word.isEmpty()) {
+      return false;
     }
+
+    List<String> listSymbols = new ArrayList<>(Arrays.stream(symbols.toLowerCase().split("")).toList());
+    List<String> listWordSymbols = new ArrayList<>(Arrays.stream(word.toLowerCase().split("")).toList());
+
+    ListIterator<String> iter = listWordSymbols.listIterator();
+    var element = "";
+
+    while (iter.hasNext()) {
+      element = iter.next();
+      if (!listSymbols.contains(element)) {
+        return false;
+      }
+      listSymbols.remove(element);
+    }
+
+    return true;
+  }
 }
 //END
