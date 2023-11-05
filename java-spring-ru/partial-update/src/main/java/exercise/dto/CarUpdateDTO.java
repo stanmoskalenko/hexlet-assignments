0ee4146945cba65691437668c024c7fb155f6962
@hexlet/cli <support@hexlet.io>
2023-11-05 15:30:51 +0300

    submit java-spring-ru/partial-update

---
+++ java-spring-ru/partial-update/src/main/java/exercise/dto/CarUpdateDTO.java
@@ -0,0 +1,15 @@
+package exercise.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.openapitools.jackson.nullable.JsonNullable;
+
+// BEGIN
+@Getter
+@Setter
+public class CarUpdateDTO {
+    private JsonNullable<String> model;
+    private JsonNullable<String> manufacturer;
+    private JsonNullable<Integer> enginePower;
+}
+// END
